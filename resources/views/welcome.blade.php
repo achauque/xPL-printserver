b511e21b5f6ab7dd5bc93f928cd7886b6bb79ff6
Chauque Esteban Alejandro <achauque@riochico.com.ar>
2020-07-01 13:27:15 -0300

    setting project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,18 +81,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    xPL-printserver
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs">xPL-printclient</a>
+                    <a href="https://www.linkedin.com/in/esteban-alejandro-chauque-7a0a3b35/">LinkedIn</a>
+                    <a href="https://github.com/achauque/">GitHub</a>
                 </div>
             </div>
         </div>
